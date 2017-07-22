041e947e45e7f387af33f6c05f3ff7fdf9f36f52
Laurent XU <laurent.xu@lrde.epita.fr>
2017-07-22 22:57:19 +0200

    add threadpool

---
+++ clftpl.hxx
@@ -0,0 +1,163 @@
+#pragma once
+#include <atomic>
+#include <future>
+#include <functional>
+#include <thread>
+#include <vector>
+#include <iostream>
+
+namespace clfctpl
+{
+  template <typename QueueType>
+  class thread_pool
+  {
+    public:
+      using func_t = std::function<void(int)>;
+
+      thread_pool(int n_threads) : n_threads_(n_threads) {init();}
+      ~thread_pool()
+      {
+        wait();
+      }
+
+      int n_waiting()
+      {
+        return threads_.size() - n_working_;
+      }
+
+      template <class Function, class... Args>
+      std::future<typename std::result_of<Function(int, Args...)>::type>
+      push(Function&& f, Args&&... args)
+      {
+        using return_t = typename std::result_of<Function(int, Args...)>::type;
+        auto f_ = std::make_shared<std::packaged_task<return_t(int)>>(
+            std::bind(std::forward<Function>(f),
+                      std::placeholders::_1,
+                      std::forward<Args>(args)...));
+        todo_queue_.push(new func_t([f_](int thread_id){(*f_)(thread_id);}));
+        return f_->get_future();
+      }
+
+      template <class Function>
+      std::future<typename std::result_of<Function(int)>::type>
+      push(Function&& f)
+      {
+        using return_t = typename std::result_of<Function(int)>::type;
+        auto f_ = std::make_shared<std::packaged_task<return_t(int)>>(
+            std::forward<Function>(f));
+        todo_queue_.push(new func_t([f_](int thread_id){(*f_)(thread_id);}));
+        return f_->get_future();
+      }
+
+      template <class Function, class... Args>
+      std::future<typename std::result_of<Function(Args...)>::type>
+      push_no_index(Function&& f, Args&&... args)
+      {
+        auto wrap = [f] (int, Args&&... args) {
+          return f(std::forward<Args>(args)...);
+        };
+        return push(wrap, std::forward<Args>(args)...);
+      }
+
+      template <class Function>
+      std::future<typename std::result_of<Function()>::type>
+      push_no_index(Function&& f)
+      {
+        auto wrap = [f] (int) {
+          return f();
+        };
+        return push(wrap);
+      }
+
+      void wait()
+      {
+        while (!todo_queue_.empty())
+          continue;
+        while (n_working_ > 0)
+          continue;
+      }
+
+#ifdef CLFCTPL_DEFINE_STOP
+      void pause()
+      {
+        is_done_ = true;
+        for (auto& t: threads_)
+          t.join();
+        threads_.clear();
+      }
+
+      void resume()
+      {
+        is_done_ = false;
+        init();
+      }
+
+      void stop()
+      {
+        pause();
+        func_t* task;
+        while(todo_queue_.pop(task))
+          continue;
+      }
+#endif
+
+    private:
+      void init()
+      {
+        threads_.reserve(n_threads_);
+        for (int i = 0; i < n_threads_; ++i)
+        {
+          auto tmp = std::thread(thread_run, std::ref(todo_queue_), i,
+                                 std::ref(is_done_), std::ref(n_working_));
+          threads_.push_back(std::move(tmp));
+        }
+      }
+
+      static void thread_run(QueueType& todo_queue, int thread_id,
+                             bool& is_done, std::atomic<int>& n_working)
+      {
+        (void) is_done;
+        func_t* todo;
+        bool is_waiting = true;
+        while (true)
+        {
+#ifdef CLFCTPL_DEFINE_STOP
+          if (is_done)
+          {
+            if (!is_waiting)
+              --n_working;
+            return;
+          }
+#endif
+          if (is_waiting)
+          {
+            if (!todo_queue.empty())
+            {
+              is_waiting = false;
+              ++n_working;
+            }
+          }
+
+          if (!is_waiting)
+          {
+            if (todo_queue.empty())
+            {
+              --n_working;
+              is_waiting = true;
+            }
+            else if (todo_queue.pop(todo))
+            {
+              (*todo)(thread_id);
+              delete todo;
+            }
+          }
+        }
+      }
+
+      std::atomic<int> n_working_;  // how many threads are waiting
+      std::vector<std::thread> threads_;
+      QueueType todo_queue_;
+      int n_threads_;
+      bool is_done_ = false;
+  };
+}
---
+++ tests/compilation/main.cc
@@ -0,0 +1,74 @@
+#define CLFCTPL_DEFINE_STOP
+#include "../../clftpl.hxx"
+#include <iostream>
+#include <boost/lockfree/queue.hpp>
+
+using func_t = std::function<void(int)>;
+using queue_type_t = boost::lockfree::queue<func_t*,
+                                            boost::lockfree::capacity<200>>;
+static clfctpl::thread_pool<queue_type_t> tp(8);
+
+static int print(int id)
+{
+  std::cout << "Hello from thread " << id << " there are " << tp.n_waiting()
+            <<  " waiting threads" <<  std::endl;
+  return id;
+}
+
+static int print2(int id, int remains)
+{
+  std::cout << "Hello from thread " << id << " still remains "
+            << remains << "calls,  there are " << tp.n_waiting()
+            <<  " waiting threads" << std::endl;
+  if (remains > 0)
+    tp.push(print2, remains - 1);
+  return id;
+}
+
+// FIXME
+static int print3(int count)
+{
+  for (; count > 0; --count)
+    std::cout << "Count down " << count << " there are " << tp.n_waiting()
+              <<  " waiting threads" << std::endl;
+  //tp.push_no_index(print3, count);
+  return 0;
+}
+
+
+static int print4()
+{
+  std::cout << "Simple test" << std::endl;
+  return 0;
+}
+
+
+int main()
+{
+  for (int i = 0; i < 50; ++i)
+  {
+    tp.push(print);
+    tp.push(print2, 10);
+  }
+  tp.wait();
+  tp.push(print2, 42);
+  tp.wait();
+  // FIXME
+  //tp.push_no_index(print3, 42);
+  //tp.stop();
+  //tp.push_no_index(print4);
+  //tp.wait();
+  tp.push(print2, 1337);
+  tp.pause();
+  std::cout << "The threadpool is paused and now we are going to resume it."
+            << std::endl;
+  tp.resume();
+  tp.push(print2, 99999);
+  tp.push(print2, 88888);
+  tp.push(print2, 77777);
+
+  tp.stop();
+  std::cout << "The threadpool is stoped." << std::endl;
+
+  return 0;
+}
